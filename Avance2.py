b30de7711ef5bf839116844d6677851fee26d473
Juan Pablo <132721368+JppRomero@users.noreply.github.com>
2025-10-22 12:35:42 -0600

    PROYECTO-TC1028

---
+++ Avance2.py
@@ -0,0 +1,20 @@
+print ( "Bienvenid@ Universitari@, al Recomendador de Películas para Universitarios. " )
+print ( "Descubre qué ver en tus ratos libres." )
+
+
+print ( "Antes de Recomendarte una Película, responde unas preguntas" )
+
+miedo_punt = int(input( "Del 0 al 100, ¿Cuánto te gustan las películas de Miedo?: " ))
+comedia_punt = int(input( "Del 0 al 100, ¿Cuánto te gustan las películas de Comedia?: " ))
+
+if miedo_punt < comedia_punt:
+        print ( "Ahhh, te gusta más la comedia!!!" )
+else:
+        print ( "Ahhh, te gusta más el miedo!!!" )
+
+
+promediopunt = ( miedo_punt + comedia_punt ) / 2
+print ( f" Tu promedio entre Miedo y Comedia es: {promediopunt} ")
+
+# La verdad no se muy bien como poner operadores al principio en mi proyecto, cuando esté más adentro si podría/
+#implementar más operadores pero al inicio, como que no la veo muy bien, :).
